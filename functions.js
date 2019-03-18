64ba67128cced5bfe815d202f40468f8b0cce4f2
Varga Zsombor <zsombi1990@gmail.com>
2019-03-18 19:39:06 +0200

    Lesson on lists. Making the unordered static list on the Skills page into an ordered dynamic list.

--- functions.js
+++ functions.js
@@ -1,3 +1,19 @@
+// -----  Colorful Browser Console Message -----
+var browserCStyle = [
+    "background: linear-gradient(#D33106, #571402)"
+    , "border: 1px solid #3E0E02"
+    , "color: white"
+    , "display: block"
+    , "text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)"
+    , "box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset"
+    , "line-height: 40px"
+    , "text-align: center"
+    , "font-weight: bold"
+].join(';');
+
+console.log("%c Contact page: form & \"Send message\" button is not yet functional! " + 
+	"Waiting for server side e-mail form scripting lesson..", browserCStyle);
+
 // ----- Left side bar content "collapsers". -----
 // Collapse-expand the "contactInfoW" (Contact Information) widget.
 var contactInfoW = document.getElementById("contactInfoW");
@@ -172,21 +188,16 @@ function showPage(page) {	// sets the visibility status of the ID'd object, reve
 
 initTopMenu();
 
+// ----- Skills' page unordered list dynamically list in ordered form -----
+function showSkills() {
+	var skills = ["html", "css", "js"];
+	for(var i = 0; i < skills.length; i++) {
+		console.info(i+1 + ". " + skills[i]);
+	}
+}
+
+showSkills();
+
 // ----- E-mail form. -----
 // ...
 
-// -----  Colorful Browser Console Message -----
-var browserCStyle = [
-    "background: linear-gradient(#D33106, #571402)"
-    , "border: 1px solid #3E0E02"
-    , "color: white"
-    , "display: block"
-    , "text-shadow: 0 1px 0 rgba(0, 0, 0, 0.3)"
-    , "box-shadow: 0 1px 0 rgba(255, 255, 255, 0.4) inset, 0 5px 3px -5px rgba(0, 0, 0, 0.5), 0 -13px 5px -10px rgba(255, 255, 255, 0.4) inset"
-    , "line-height: 40px"
-    , "text-align: center"
-    , "font-weight: bold"
-].join(';');
-
-console.log("%c Contact page: form & \"Send message\" button is not yet functional! " + 
-	"Waiting for server side e-mail form scripting lesson..", browserCStyle);
